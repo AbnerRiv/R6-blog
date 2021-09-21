ec7cedf9343d19f3dd957e1bef832f7d0aedf8f7
AbnerRiv <alimohamend16@gmail.com>
2021-09-21 10:05:04 -0600

    crud in record associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    # for comments you do ne the post/:post_id/comment for every route
+    # so you can nest it like this
+    resources :comments
+  end
 
   resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
